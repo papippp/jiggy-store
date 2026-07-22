ca71d0fc6fb0f1394899777af3eb23e55aa942df
papi <dlukzy@gmail.com>
2026-07-23 06:52:35 +1400

    fixed favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
 import ToastProvider from "./components/ToastProvider"
-import AuthPage from "./pages/AuthPage"
+
 import CheckoutPage from "./pages/CheckoutPage"
 import HomePage from "./pages/HomePage"
 import Men from "./pages/Men"
@@ -22,27 +22,26 @@ function App() {
 
   return (
 
-    
-      <Provider store={store}>
-        <BrowserRouter>
-        <ToastProvider/>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/product/:id" element={<ProductDetail/>}/>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/track" element={<TrackOrder/>}/>
-            <Route path="*" element={<LandingPage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/men" element={<Men />} />
-            <Route path="/women" element={<Women />} />
-            <Route path='/admin' element={<AdminDashboard/>}/>
-            <Route path="/checkout" element={<CheckoutPage />} />
-          </Routes>
 
-        </BrowserRouter>
-      </Provider>
-    
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToastProvider />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/track" element={<TrackOrder />} />
+          <Route path="*" element={<LandingPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/men" element={<Men />} />
+          <Route path="/women" element={<Women />} />
+          <Route path='/admin' element={<AdminDashboard />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+
+      </BrowserRouter>
+    </Provider>
+
 
   )
 }
